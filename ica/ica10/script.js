1c48c4aaa82b79a25c9d17d86589e579ec5aa5a5
SarahSharroufna <sash1048@colorado.edu>
2024-06-30 22:21:31 -0600

    wa10

---
+++ ica/ica10/script.js
@@ -0,0 +1,15 @@
+
+let button = document.getElementById("color-btn");
+
+    button.addEventListener("click", function() {
+        //changes the bg color to red
+        document.body.style.backgroundColor = "skyblue";
+    });
+
+
+let square = document.getElementById("square");
+
+div.addEventListener("click", function() {
+    //changes the bg color to red
+    square.style.borderRadius = 50 + "px";
+});
