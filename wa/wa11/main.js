18347bfd00e845088180e9daf8b0e0f875a86416
SarahSharroufna <sash1048@colorado.edu>
2024-07-01 07:05:26 -0600

    image size problem

--- wa/wa11/main.js
+++ wa/wa11/main.js
@@ -6,16 +6,16 @@ const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
 
-const images = ['pic1.jpeg', `pic2.webp`, `pic3.webp`, `pic4.webp`, `pic5.jpeg`];
+const images = ['pic1.jpg', `pic2.jpg`, `pic3.jpg`, `pic4.jpg`, `pic5.jpg`];
 const alts = {
-  'pic1.jpeg' : 'Pancakes',
-  'pic2.webp' : 'Eggs with toast',
-  'pic3.webp' : 'Fruit Tart',
-  'pic4.webp' : 'Crepes with banans',
-  'pic5.jpeg' : 'Chocklate cake'
+  'pic1.jpg' : 'Closeup of a human eye',
+  'pic2.jpg' : 'Rock that looks like a wave',
+  'pic3.jpg' : 'Purple and white pansies',
+  'pic4.jpg' : 'Section of wall from a pharoah\'s tomb',
+  'pic5.jpg' : 'Large moth on a leaf'
 }
 
-/* Looping through images */
+/* Looping the images */
 
 for (const image of images) {
   const newImage = document.createElement('img');
@@ -28,7 +28,7 @@ for (const image of images) {
   });
 }
 
-/* Wiring up the Darken/Lighten button */
+/* For the Button */
 
 btn.addEventListener('click', () => {
   const btnClass = btn.getAttribute('class');
